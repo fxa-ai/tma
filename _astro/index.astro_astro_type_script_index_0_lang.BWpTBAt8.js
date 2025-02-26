c064f3b3b13ac0ee7aca5f7a30abb3ef57cf0536
Mahdi <mkfard@outlook.com>
2025-02-26 16:58:25 +0330

    .

---
+++ _astro/index.astro_astro_type_script_index_0_lang.BWpTBAt8.js
@@ -0,0 +1,1 @@
+import{W as a}from"./index.BtZ2aNdV.js";a.downloadFile({url:"https://fxa.app/favicon.png",file_name:"favicon.png"});alert(a.initData);
--- _astro/index.astro_astro_type_script_index_0_lang.C5PJ3De6.js
+++
@@ -1,1 +0,0 @@
-import{W as a}from"./index.BtZ2aNdV.js";alert(a.initData);
